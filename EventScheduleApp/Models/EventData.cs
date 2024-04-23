10c60ac78d19938ebfe8928e219245fe523d97fa
Anuja Nisal Yashohara <rmanuja2003@gmail.com>
2024-04-23 10:23:25 +0530

    firebase data add done

---
+++ EventScheduleApp/Models/EventData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventScheduleApp.Models
+{
+    public class EventData
+    {
+        public string Event { get; set; }
+        public string Date { get; set; }
+        public string EventID { get; internal set; }
+
+        public override string ToString()
+        {
+            return $"Event: {Event}, Date: {Date}";
+        }
+    }
+}
--- EventScheduleApp/Views/AddEventForm.cs
+++ EventScheduleApp/Views/AddEventForm.cs
@@ -1,7 +1,11 @@
-﻿using EventScheduleApp.Views.UserControls;
+﻿using EventScheduleApp.Models;
+using EventScheduleApp.Views.UserControls;
+using Google.Cloud.Firestore;
 using MySqlConnector;
 using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace EventScheduleApp.Views
@@ -9,11 +13,22 @@ namespace EventScheduleApp.Views
     public partial class AddEventForm : Form
     {
         public event EventHandler<EventAddedEventArgs> EventAdded;
-        MySqlConnection connection = new MySqlConnection("server=localhost;database=eventdb;port=3306;username=root;password=");
+        private FirestoreDb db;
+
+        
 
         public AddEventForm()
         {
             InitializeComponent();
+            InitializeFirestore();
+        }
+
+        private void InitializeFirestore()
+        {
+            // Change the credentials file path to the path of your own credentials file
+            string projectId = "testeventlast";
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "C:\\Users\\User\\Desktop\\Studee\\EventScheduleApp\\EventScheduleApp\\FirebaseCred.json");
+            db = FirestoreDb.Create(projectId);
         }
 
         private void AddEventForm_Load(object sender, EventArgs e)
@@ -21,35 +36,52 @@ namespace EventScheduleApp.Views
             txdate.Text = EventDashboard.static_month + "/" + UserControlDay.static_day + "/" + EventDashboard.static_year;
         }
 
-        private void btnSave_Click(object sender, EventArgs e)
+        private string GenerateTaskID()
         {
+            // Generate a unique task ID using timestamp and a random number
+            string timestamp = System.DateTime.UtcNow.ToString("yyyyMMddHHmmssfff");
+            string randomNumber = new Random().Next(1000, 9999).ToString();
+            string taskID ="event" + timestamp + randomNumber;
+            return taskID;
+        }
+
+
+        private async void btnSave_Click(object sender, EventArgs e)
+        {
+
+            EventData EventData = new EventData
+            {
+                EventID = GenerateTaskID(),
+                Event = txevent.Text,
+                Date = txdate.Text,
+            };
+
+            // Raise the EventAdded event and pass event name and date
+            EventAdded?.Invoke(this, new EventAddedEventArgs(txevent.Text, txdate.Text));
+
+            var EventItemData = new Dictionary<string, object>
+            {
+                { "EventID", EventData.EventID },
+                { "Event", EventData.Event },
+                { "Date", EventData.Date },
+            };
             try
             {
-                connection.Open();
+                // Ensure that userLoggedEmail is replaced with the actual user's email
+                DocumentReference eventDoc = db.Collection("userLoggedEmail").Document(EventData.EventID);
 
-                string iquery = "INSERT INTO eventdb.users(`ID`, `Event`, `Date`) VALUES(NULL, @Event, @Date)";
+                // Pass EventItemData instead of EventData
+                await eventDoc.SetAsync(EventItemData);
 
-                MySqlCommand commandDatabase = new MySqlCommand(iquery, connection);
-                commandDatabase.Parameters.AddWithValue("@Event", txevent.Text);
-                commandDatabase.Parameters.AddWithValue("@Date", txdate.Text);
-
-                commandDatabase.ExecuteNonQuery();
-                MessageBox.Show("Data Added Successful", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-                //need to pass Event Name and Date to UserControlDay class
-                // Raise the EventAdded event and pass event name and date
-                EventAdded?.Invoke(this, new EventAddedEventArgs(txevent.Text, txdate.Text));
-
-                this.Hide();
+                MessageBox.Show("Event Added Successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Error adding event: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            finally
-            {
-                connection.Close();
-            }
+
+
         }
     }
 }
